a66e93b16ac558647db25e89a301478db0bb51a4
jc-gh25 <jeff@jcarl.net>
2025-11-19 01:06:01 -0700

    Edit CorsConfig

--- music-library/src/main/java/music/library/config/CorsConfig.java
+++ music-library/src/main/java/music/library/config/CorsConfig.java
@@ -2,11 +2,10 @@ package music.library.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
 	// Allows the Swagger UI (and any client run locally) to call the API.
